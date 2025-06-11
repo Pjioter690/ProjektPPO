d197f06d12e8324852b72c182186259506281a48
ajSzok <ajSzok@gmail.com>
2025-06-11 08:52:55 +0200

    Update weapon

--- WeaponAttack.cpp
+++ WeaponAttack.cpp
@@ -5,7 +5,7 @@ void Weapon::attack(float d,std::vector<std::unique_ptr<Enemy>>& enemies){
     if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
         for (auto& enemy : enemies) {
             if (weaponHitbox.getGlobalBounds().intersects(enemy->getGlobalBounds())) {
-                enemy->dealDmg(d);
+                enemy->dealDmg(d*this->dmg);
             }
         }
     }
